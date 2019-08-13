21eed882a337a1ff35f1b1f4c480f9275f1e7c87
Anderson Nunes <anderson.nunes1988@gmail.com>
2019-08-13 10:13:48 -0300

    Styling Splash Page / App Cleanup

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,7 +1,7 @@
-export default function reducer(state, action) {
-  switch (action.type) {
+export default function reducer(state, { type, payload }) {
+  switch (type) {
     case "LOGIN_USER":
-      return { ...state, currentUser: action.payload };
+      return { ...state, currentUser: payload };
     default:
       return state;
   }
