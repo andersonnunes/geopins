8f92e30ba4e328575b2c23513e45f3f11682d6f7
Anderson Nunes <anderson.nunes1988@gmail.com>
2019-08-13 11:34:20 -0300

    Build Signout Button

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -4,6 +4,8 @@ export default function reducer(state, { type, payload }) {
       return { ...state, currentUser: payload };
     case "IS_LOGGED_IN":
       return { ...state, isAuth: payload };
+    case "SIGNOUT_USER":
+      return { ...state, isAuth: false, currentUser: null };
     default:
       return state;
   }
